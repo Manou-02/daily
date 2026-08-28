1b578814e982f51972c11408a6bd288b23c6e3ab
Manou <mrakotoarivelo97@gmail.com>
2026-08-28 20:37:34 +0300

    INit nativeWind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,11 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Some changes.</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-red-400">
+        Welcome to Nativewind!
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
