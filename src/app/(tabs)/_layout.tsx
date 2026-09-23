4986c81f09543f5c253773e3a4679017779c7f20
Manoa <manoa@livenexx.net>
2026-09-23 16:20:24 +0300

    Create tab bar with icon

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,18 +1,25 @@
-import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { router, Tabs } from "expo-router";
 import { useUnstableNativeVariable } from "nativewind";
+import { Pressable } from "react-native";
 
 export default function TabLayout() {
   const primary = useUnstableNativeVariable("--color-primary");
   const surface = useUnstableNativeVariable("--color-surface");
   const border = useUnstableNativeVariable("--color-border");
   const textSecondary = useUnstableNativeVariable("--color-textSecondary");
+
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
         tabBarStyle: {
           backgroundColor: surface,
-          borderTopColor: border,
+          borderRadius: 50,
+          marginHorizontal: 20,
+          marginBottom: 20,
+          position: "absolute",
+          borderTopWidth: 0,
         },
 
         tabBarActiveTintColor: primary,
@@ -23,12 +30,51 @@ export default function TabLayout() {
         name="home"
         options={{
           title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="add"
+        options={{
+          title: "",
+          tabBarButton: () => (
+            <Pressable
+              onPress={() => {
+                router.push("/add");
+              }}
+              style={{
+                width: 60,
+                height: 60,
+                borderRadius: 30,
+                backgroundColor: primary,
+                alignItems: "center",
+                justifyContent: "center",
+                marginTop: -15,
+              }}
+            >
+              <Ionicons name="add" size={30} color="white" />
+            </Pressable>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="budgets"
+        options={{
+          title: "Budgets",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="wallet-outline" size={size} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="settings"
         options={{
-          title: "Setting",
+          title: "Paramètres",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings-outline" size={size} color={color} />
+          ),
         }}
       />
     </Tabs>
