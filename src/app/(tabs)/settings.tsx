10fcf03fbd652a2bed18b52a33e75d8722342ca3
Manoa <manoa@livenexx.net>
2026-09-24 12:18:11 +0300

    WIP

--- src/app/(tabs)/settings.tsx
+++ src/app/(tabs)/settings.tsx
@@ -9,6 +9,8 @@ import { Text, View } from "react-native";
 export default function Settings() {
   const { theme, setTheme } = useTheme();
     const textPrimary = useUnstableNativeVariable("--color-textPrimary");
+    const textSecondary = useUnstableNativeVariable("--color-textSecondary");
+  const surface = useUnstableNativeVariable("--color-surfaceVariant");
   
 
   const themeOptions = [
@@ -30,7 +32,19 @@ export default function Settings() {
   ] as const;
 
   return (
-    <View>
+    <View className="gap-4">
+      {/* Theme configuration  */}
+      <View className="justify-between flex-row my-2">
+        <Text className="text-textPrimary text-2xl"> Paramètres </Text>
+        <View>
+
+        <Ionicons
+                name={"settings"}
+                size={size.iconSize}
+                color={textSecondary}
+              />
+        </View>
+      </View>
       <Card>
         <View className="mb-4 flex-row gap-2 ">
           <View className="p-2 bg-primaryContainer rounded">
@@ -50,6 +64,30 @@ export default function Settings() {
             value={theme}
             onChange={setTheme}
           />
+        </View>
+      </Card>
+
+      {/* Langage configuration  */}
+
+      <Card>
+        <View className="mb-4 flex-row gap-2 ">
+          <View className="p-2 bg-border rounded">
+             <Ionicons
+                name={"globe-outline"}
+                size={size.iconSize}
+                color={textSecondary}
+              />
+          </View>
+          <Text className="text-2xl text-textPrimary"> Langue </Text>
+        </View>
+        <View className="flex-row justify-between">
+          <View> 
+            <Text className="text-textPrimary text-xl"> Langue de l'app </Text>
+            <Text className="text-textPrimary text-sm"> Interface système </Text>
+          </View>
+          <View>
+            <Text className="text-textPrimary">zaza</Text>
+          </View>
         </View>
       </Card>
     </View>
