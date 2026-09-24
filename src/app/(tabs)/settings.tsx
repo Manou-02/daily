2e291275cd752157e72604d49ca1f6d3507d6bc2
Manoa <manoa@livenexx.net>
2026-09-24 12:30:12 +0300

    Add catégories sections

--- src/app/(tabs)/settings.tsx
+++ src/app/(tabs)/settings.tsx
@@ -4,13 +4,13 @@ import { size } from "@/themes/size";
 import { useTheme } from "@/themes/ThemeProvider";
 import { Ionicons } from "@expo/vector-icons";
 import { useUnstableNativeVariable } from "nativewind";
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 
 export default function Settings() {
   const { theme, setTheme } = useTheme();
     const textPrimary = useUnstableNativeVariable("--color-textPrimary");
     const textSecondary = useUnstableNativeVariable("--color-textSecondary");
-  const surface = useUnstableNativeVariable("--color-surfaceVariant");
+  const warning = useUnstableNativeVariable("--color-warning");
   
 
   const themeOptions = [
@@ -33,7 +33,6 @@ export default function Settings() {
 
   return (
     <View className="gap-4">
-      {/* Theme configuration  */}
       <View className="justify-between flex-row my-2">
         <Text className="text-textPrimary text-2xl"> Paramètres </Text>
         <View>
@@ -42,12 +41,43 @@ export default function Settings() {
                 name={"settings"}
                 size={size.iconSize}
                 color={textSecondary}
-              />
+                />
         </View>
       </View>
+
+     {/* Catégories configuration  */}
       <Card>
         <View className="mb-4 flex-row gap-2 ">
-          <View className="p-2 bg-primaryContainer rounded">
+          <View className="p-2 bg-border rounded-md">
+             <Ionicons
+                name={"shapes-outline"}
+                size={size.iconSize}
+                color={warning}
+              />
+          </View>
+          <Text className="text-2xl text-textPrimary"> Catègories </Text>
+        </View>
+        <View className="gap-3">
+          <Pressable            
+            className="flex-row rounded-xl p-2 bg-surfaceVariant justify-center gap-2"
+            onPress={() => {
+              console.log("zazazaa")
+            }}
+          >
+                <Ionicons
+                name={"add-circle-outline"}
+                size={size.iconSize}
+                color={textSecondary}
+              />
+            <Text className="text-textSecondary">Ajouter une catégorie personnalisé</Text>
+          </Pressable>
+        </View>
+      </Card>
+
+      {/* Theme configuration  */}
+      <Card>
+        <View className="mb-4 flex-row gap-2 ">
+          <View className="p-2 bg-primaryContainer rounded-md">
              <Ionicons
                 name={"color-palette-outline"}
                 size={size.iconSize}
@@ -71,7 +101,7 @@ export default function Settings() {
 
       <Card>
         <View className="mb-4 flex-row gap-2 ">
-          <View className="p-2 bg-border rounded">
+          <View className="p-2 bg-border rounded-md">
              <Ionicons
                 name={"globe-outline"}
                 size={size.iconSize}
